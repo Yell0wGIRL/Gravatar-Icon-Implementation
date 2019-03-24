2fea6873ba25b8dd48c4589eff62deaa95634856
YellowGIRL <42133415+Yell0wGIRL@users.noreply.github.com>
2019-03-23 23:22:52 -0700

    Create gravatar.js

---
+++ src/js/gravatar.js
@@ -0,0 +1,10 @@
+function getGravatar(email, size=80, d='pm', rating='g') {
+  var s = document.createElement('script')
+  s.src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"
+  document.append(s)
+  
+  var mdEmail = CryptoJS.MD5(email)
+  var url = "https://s.gravatar.com/avatar/"+mdEmail+"?s="+size+"&d="+d+"&r="+rating
+  var pic = document.getElementById("profile-picture")
+  pic.src = url
+}
